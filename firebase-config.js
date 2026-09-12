b401f1bf4fe388b486713d7311acfccff2c310cb
1rteus <1rteus@users.noreply.github.com>
2026-09-12 10:38:25 +0300

    восстановить firebase-config.js для GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+var FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD8ibGJKw_sb7-7VTE6WTxQGd6uUiXbncI",
+    authDomain: "colleg-e3f07.firebaseapp.com",
+    projectId: "colleg-e3f07",
+    storageBucket: "colleg-e3f07.firebasestorage.app",
+    messagingSenderId: "124317184487",
+    appId: "1:124317184487:web:f66ab2705e00fe3c02cd42"
+};
